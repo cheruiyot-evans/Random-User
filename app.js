623a6080636cd06856c3c705149918336d8df70b
Cheruiyot Evans <chersoftwaredev@gmail.com>
2026-02-03 06:31:08 +0300

    displayUser fetchUser getElement removeActive modules

--- app.js
+++ app.js
@@ -1,46 +1,8 @@
 import { getElement } from './utils/getElement.js';
 import getUser from './utils/fetchUser.js';
+import displayUser from './utils/displayUser.js';
 
-const img = getElement('.user-img');
-const title = getElement('.user-title');
-const value = getElement('.user-value');
 const btn = getElement('.btn');
-const btns = [...document.querySelectorAll('.icon')];
-
-const displayUser = (person) => {
-  img.src = person.image;
-  value.textContent = person.name;
-  title.textContent = `My name is`;
-  btns[0].classList.add('active');
-  btns.map((btn) => {
-    const label = btn.dataset.label;
-    if (label === 'name') {
-      btn.addEventListener('click', () => {
-        value.textContent = person.name;
-      });
-    } else if (label === 'email') {
-      btn.addEventListener('click', () => {
-        value.textContent = person.email;
-      });
-    } else if (label === 'street') {
-      btn.addEventListener('click', () => {
-        value.textContent = person.street;
-      });
-    } else if (label === 'age') {
-      btn.addEventListener('click', () => {
-        value.textContent = person.age;
-      });
-    } else if (label === 'phone') {
-      btn.addEventListener('click', () => {
-        value.textContent = person.phone;
-      });
-    } else if (label === 'password') {
-      btn.addEventListener('click', () => {
-        value.textContent = person.password;
-      });
-    }
-  });
-};
 
 // show user
 const showUser = async () => {
---
+++ utils/removeActive.js
@@ -0,0 +1,4 @@
+function removeActive(items) {
+  items.forEach((btn) => btn.classList.remove('active'));
+}
+export default removeActive;
